c6f05cfd87d1a43b8a1305a1db55d0231b8915c0
kana686 <inui.k.w6@gmail.com>
2026-07-23 11:17:02 +0900

    feat: 出版日にY-m-dフォーマットを設定 #4

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -21,6 +23,13 @@ class Book extends Model
         'description',
         'image_url',
     ];
+
+    protected function publishedDate(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value ? Carbon::parse($value)->format('Y-m-d') : null,
+        );
+    }
 
     public function user(): BelongsTo
     {
