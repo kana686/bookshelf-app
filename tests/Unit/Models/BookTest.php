2541bea933d99cf40d9f3d3f69d7c09481b4d47c
kana686 <inui.k.w6@gmail.com>
2026-07-25 13:32:27 +0900

    feat: BookTest(単体テスト)を作成 #8

---
+++ tests/Unit/Models/BookTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Book;
+use PHPUnit\Framework\TestCase;
+
+class BookTest extends TestCase
+{
+    public function test_fillableに設定された属性が一括代入できる()
+    {
+        $book = new Book([
+            'title' => 'テストタイトル',
+            'author' => 'テスト著者',
+            'isbn' => '9784101010999',
+            'published_date' => '2026-01-01',
+            'description' => 'テスト用の説明文',
+        ]);
+
+        $this->assertEquals('テストタイトル', $book->title);
+        $this->assertEquals('テスト著者', $book->author);
+        $this->assertEquals('9784101010999', $book->isbn);
+        $this->assertEquals('2026-01-01', $book->published_date);
+        $this->assertEquals('テスト用の説明文', $book->description);
+    }
+}
