c7389e9adbcdfddd1bc86cd78348294095d72492
kana686 <inui.k.w6@gmail.com>
2026-07-27 18:05:39 +0900

    refactor: 会員登録のルート名をregisterに修正 #13

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(RegisteredUserController::class)->group(function () {
-    Route::get('/register', 'create')->name('register.create');
-    Route::post('/register', 'store')->name('register.store');
+    Route::get('/register', 'create')->name('register');
+    Route::post('/register', 'store')->name('register');
 });
 
 Route::controller(AuthenticatedSessionController::class)->group(function () {
