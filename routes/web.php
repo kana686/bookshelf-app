6bc2bd719a96903f416449f5b32e88acc6ac27fd
kana686 <inui.k.w6@gmail.com>
2026-07-27 18:04:42 +0900

    feat: AuthenticatedSessionControllerの作成とルートの設定 #13

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,8 @@ Route::controller(RegisteredUserController::class)->group(function () {
     Route::get('/register', 'create')->name('register.create');
     Route::post('/register', 'store')->name('register.store');
 });
+
+Route::controller(AuthenticatedSessionController::class)->group(function () {
+    Route::get('/login', 'create')->name('login');
+    Route::post('/login', 'store')->name('login');
+});
