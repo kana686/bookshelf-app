36bc17e45760f2a121f8c4a72ed9040358ac8680
kana686 <inui.k.w6@gmail.com>
2026-07-28 15:26:19 +0900

    fix: /loginのルートにguestミドルウェアを適用 #13

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,9 @@ Route::controller(RegisteredUserController::class)->group(function () {
     Route::post('/register', 'store')->name('register');
 });
 
-Route::controller(AuthenticatedSessionController::class)->group(function () {
-    Route::get('/login', 'create')->name('login');
-    Route::post('/login', 'store')->name('login');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
+    Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 });
 
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
