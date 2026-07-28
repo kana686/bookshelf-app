be1c3996d777a6574d21f296ee39989b30c8b4a5
kana686 <inui.k.w6@gmail.com>
2026-07-28 17:46:11 +0900

    feat: BookServiceにデータ取得ロジックを記載 #15

---
+++ app/Services/BookService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Book;
+
+class BookService
+{
+    public function getPaginatedBooks(int $perPage = 10)
+    {
+        return Book::with('genres')->latest()->paginate($perPage);
+    }
+
+    public function getBookById(int $id)
+    {
+        return Book::with(['genres', 'reviews.user', 'favorites'])->findOrFail($id);
+    }
+}
