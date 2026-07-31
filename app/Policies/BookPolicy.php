344d09d93324991c4dbec7462694bea43536d2e7
kana686 <inui.k.w6@gmail.com>
2026-07-31 15:20:58 +0900

    feat: BookPolicyにurlで直接アクセスした時用のフラッシュメッセージを設定 #18

--- app/Policies/BookPolicy.php
+++ app/Policies/BookPolicy.php
@@ -4,16 +4,21 @@ namespace App\Policies;
 
 use App\Models\Book;
 use App\Models\User;
+use Illuminate\Auth\Access\Response;
 
 class BookPolicy
 {
-    public function update(User $user, Book $book): bool
+    public function update(User $user, Book $book): Response
     {
-        return $user->id === $book->user_id;
+        return $user->id === $book->user_id
+            ? Response::allow()
+            : Response::deny('自身の投稿した書籍のみ編集できます');
     }
 
-    public function delete(User $user, Book $book): bool
+    public function delete(User $user, Book $book): Response
     {
-        return $user->id === $book->user_id;
+        return $user->id === $book->user_id
+            ? Response::allow()
+            : Response::deny('自身の投稿した書籍のみ削除できます');
     }
 }
