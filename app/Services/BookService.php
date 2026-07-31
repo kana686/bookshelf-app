57b75b508b917a2bd3e15bf16baca597cebd3cca
kana686 <inui.k.w6@gmail.com>
2026-07-31 14:30:53 +0900

    feat: BookServiceに登録・編集・削除の処理を追加 #18

--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -15,4 +15,33 @@ class BookService
     {
         return Book::with(['genres', 'reviews.user', 'usersWhoFavorited'])->findOrFail($id);
     }
+
+    public function createBook(array $data): Book
+    {
+        $data['user_id'] = Auth::id();
+
+        $book = Book::create($data);
+
+        if (isset($data['genres'])) {
+            $book->genres()->sync($data['genres']);
+        }
+
+        return $book;
+    }
+
+    public function updateBook(Book $book, array $data): Book
+    {
+        $book->update($data);
+
+        if (isset($data['genres'])) {
+            $book->genres()->sync($data['genres']);
+        }
+
+        return $book;
+    }
+
+    public function deleteBook(Book $book): void
+    {
+        $book->delete();
+    }
 }
