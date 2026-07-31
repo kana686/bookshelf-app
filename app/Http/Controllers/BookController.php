740ad79ac275abffbbb38b40ba9fe8fb3b42f817
kana686 <inui.k.w6@gmail.com>
2026-07-31 14:36:39 +0900

    feat: BookControllerにcreate(),store(),edit(),update(),destroy()を追加 #18

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BookRequest;
 use App\Models\Book;
 use App\Services\BookService;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class BookController extends Controller
 {
+    use AuthorizesRequests;
+
     protected BookService $bookService;
 
     public function __construct(BookService $bookService)
@@ -26,5 +30,45 @@ class BookController extends Controller
         $book = $this->bookService->getBookById($book->id);
 
         return view('books.show', compact('book'));
+    }
+
+    public function create()
+    {
+        return view('books.create');
+    }
+
+    public function store(BookRequest $request)
+    {
+        $book = $this->bookService->createBook($request->validated());
+
+        return redirect()->route('books.show', $book)
+            ->with('success', '書籍を登録しました');
+    }
+
+    public function edit(Book $book)
+    {
+        $this->authorize('update', $book);
+
+        return view('books.edit', compact('book'));
+    }
+
+    public function update(BookRequest $request, Book $book)
+    {
+        $this->authorize('update', $book);
+
+        $this->bookService->updateBook($book, $request->validated());
+
+        return redirect()->route('books.show', $book)
+            ->with('success', '書籍情報を更新しました');
+    }
+
+    public function destroy(Book $book)
+    {
+        $this->authorize('delete', $book);
+
+        $this->bookService->deleteBook($book);
+
+        return redirect()->route('books.index')
+            ->with('success', '書籍を削除しました');
     }
 }
