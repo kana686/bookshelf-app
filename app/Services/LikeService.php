b976aecc481108c1c215413d28062d6cb54b1639
kana686 <inui.k.w6@gmail.com>
2026-08-02 17:28:26 +0900

    fix: メソッド名がモデルと整合していなかったため修正 #22

--- app/Services/LikeService.php
+++ app/Services/LikeService.php
@@ -9,10 +9,10 @@ class LikeService
 {
     public function toggleLike(Review $review, Authenticatable $user): void
     {
-        if ($review->likes()->where('user_id', $user->id)->exists()) {
-            $review->likes()->where('user_id', $user->id)->delete();
+        if ($review->likedByUsers()->where('user_id', $user->id)->exists()) {
+            $review->likedByUsers()->where('user_id', $user->id)->delete();
         } else {
-            $review->likes()->create([
+            $review->likedByUsers()->create([
                 'user_id' => $user->id,
             ]);
         }
