d3645c0bb812001f3cde4c089fb58bf8f6c223b6
kana686 <inui.k.w6@gmail.com>
2026-08-03 16:58:49 +0900

    feat: FavoriteControllerとFavoriteServiceにお気に入り一覧表示の処理を追加 #26 - FavoriteControllerにindex()を追加 - FavoriteServiceにお気に入りに追加している書籍を取得する処理とページネーションの処理を追加

--- app/Services/FavoriteService.php
+++ app/Services/FavoriteService.php
@@ -4,9 +4,17 @@ namespace App\Services;
 
 use App\Models\Book;
 use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class FavoriteService
 {
+    public function getPaginatedFavorites(Authenticatable $user, int $perPage = 10): LengthAwarePaginator
+    {
+        return $user->favoriteBooks()
+            ->latest()
+            ->paginate($perPage);
+    }
+
     public function toggleFavorite(Book $book, Authenticatable $user): void
     {
         $book->usersWhoFavorited()->toggle($user->id);
