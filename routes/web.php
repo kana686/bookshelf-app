837ac0d55dc36155e762d32725d27a6226eb69c8
kana686 <inui.k.w6@gmail.com>
2026-08-05 16:46:44 +0900

    feat: ジャンル一覧・詳細画面のルートを設定し仮ルートを削除 #28

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthenticatedSessionController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\GenreController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\ReviewController;
@@ -46,9 +47,10 @@ Route::middleware('auth')->group(function () {
         Route::delete('/reviews/{review}', 'destroy')->name('reviews.destroy');
     });
 
-    Route::get('/genres', function () {
-        return 'ジャンル一覧画面';
-    })->name('genres.index'); // 仮ルート
+    Route::controller(GenreController::class)->prefix('genres')->group(function () {
+        Route::get('', 'index')->name('genres.index');
+        Route::get('{genre}', 'show')->name('genres.show');
+    });
 });
 
 // 公開
