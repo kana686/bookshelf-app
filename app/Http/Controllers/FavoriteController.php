ddf51573f887487c322c950bbb580b98b9d96add
kana686 <inui.k.w6@gmail.com>
2026-08-10 16:34:30 +0900

    refactor: FavoriteControllerの__construct の配置場所を修正とメソッドの引数整理 #34

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -12,16 +12,16 @@ class FavoriteController extends Controller
 {
     protected FavoriteService $favoriteService;
 
-    public function index(Request $request, FavoriteService $favoriteService): View
-    {
-        $books = $favoriteService->getPaginatedFavorites($request->user());
-
-        return view('favorites.index', compact('books'));
-    }
-
     public function __construct(FavoriteService $favoriteService)
     {
         $this->favoriteService = $favoriteService;
+    }
+
+    public function index(Request $request): View
+    {
+        $books = $this->favoriteService->getPaginatedFavorites($request->user());
+
+        return view('favorites.index', compact('books'));
     }
 
     public function store(Request $request, Book $book): RedirectResponse
