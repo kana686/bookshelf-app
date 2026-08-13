ffec06a459315f964973ab6e079475b408efb150
kana686 <inui.k.w6@gmail.com>
2026-08-13 15:05:36 +0900

    feat: Api/V1/BookControllerにstore(),update(),destroy()を追加 #41

--- app/Http/Controllers/Api/V1/BookController.php
+++ app/Http/Controllers/Api/V1/BookController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\ApiBookRequest;
 use App\Http\Requests\Api\V1\BookSearchRequest;
 use App\Http\Resources\BookResource;
+use App\Models\Book;
 use App\Services\BookService;
 use Illuminate\Http\JsonResponse;
 
@@ -33,5 +35,32 @@ class BookController extends Controller
         $book = $this->bookService->getBookById($id);
 
         return (new BookResource($book))->response();
+    }
+
+    public function store(ApiBookRequest $request): JsonResponse
+    {
+        $data = $request->validated();
+
+        $book = $this->bookService->createBook($data);
+
+        return (new BookResource($book))->response()->setStatusCode(201);
+    }
+
+    public function update(ApiBookRequest $request, int $id): JsonResponse
+    {
+        $book = Book::findOrFail($id);
+
+        $updatedBook = $this->bookService->updateBook($book, $request->validated());
+
+        return (new BookResource($updatedBook))->response();
+    }
+
+    public function destroy(int $id): JsonResponse
+    {
+        $book = Book::findOrFail($id);
+
+        $this->bookService->deleteBook($book);
+
+        return response()->json(null, 204);
     }
 }
